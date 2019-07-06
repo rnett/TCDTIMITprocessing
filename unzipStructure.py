0401a9b4a038c4e6fc81080159b99396b6f36ad1
Ryan Nett <rnett@skymind.io>
2019-07-06 16:37:57 -0700

    fix

--- unzipStructure.py
+++ unzipStructure.py
@@ -1,34 +1,30 @@
 import concurrent.futures
 import os.path
 
-from utils.helpFunctions import *
-
-
 # Some helper functions
-def silentremove(filename):
+from utils.helpFunctions import query_yes_no
+
+
+def silentremove (filename):
     try:
         os.remove(filename)
     except OSError as e:  # name the Exception `e`
-        pass  # print("Failed with:", e.strerror)  # look what it says
+        pass #print("Failed with:", e.strerror)  # look what it says
     return 0
-
-
+    
 # create a list with paths of all zip files
 def createZipList(rootDir):
-    zipList = []
+    zipList= []
     for root, dirs, files in os.walk(rootDir):
         for fname in files:
             if ".zip" in fname:
                 path = ''.join([root, os.sep, fname])
-                # print('Found zip: %s' % path)
+                #print('Found zip: %s' % path)
                 zipList.append(path)
     return zipList
 
-
 # define func to unzip one file
 import zipfile
-
-
 def extractZip(filePath, delete=False):
     targetDir = os.path.dirname(filePath)
     with zipfile.ZipFile(filePath, "r") as z:
@@ -36,47 +32,43 @@ def extractZip(filePath, delete=False):
     if (delete):
         silentremove(filePath)
     return 0
-
-
+    
 ######################################
 ########## Main Function #############
 ######################################
 
 rootDir = "/home/rnett/Desktop/TCDTIMIT/data/volunteers"
-batchSize = 1  # 1 or 2 recommended
+batchSize = 1 # 1 or 2 recommended
 
-zipList = createZipList(rootDir)
+zipList= createZipList(rootDir)
 print("\n".join(zipList))
 
-if query_yes_no("Would you like to process these zip files?", "yes"):
-    deleteZips = query_yes_no(
-        "Would you like to remove the zip files after extraction?", "no")
-    batchIndex = 0
-    executor = concurrent.futures.ProcessPoolExecutor(batchSize)
-    running = 1
-    while running:
-        # get commands for current batch
-        if batchIndex + batchSize > len(zipList):
-            print("Processing LAST BATCH...")
-            running = 0
-            currentZips = zipList[batchIndex:]  # till the end
-        else:
-            currentZips = zipList[batchIndex:batchIndex + batchSize]
+deleteZips = True
+batchIndex = 0
+executor = concurrent.futures.ProcessPoolExecutor(batchSize)
+running = 1
 
-        # execute the commands
-        futures = []
-        for i in range(len(currentZips)):
-            filePath = currentZips[i]
-            print("Unzipping ", filePath)
-            futures.append(executor.submit(extractZip, filePath, deleteZips))
-        concurrent.futures.wait(futures)
+while running:
+    # get commands for current batch
+    if batchIndex + batchSize > len(zipList):
+        print("Processing LAST BATCH...")
+        running = 0
+        currentZips = zipList[batchIndex:]  # till the end
+    else:
+        currentZips = zipList[batchIndex:batchIndex + batchSize]
 
-        # update the batchIndex
-        batchIndex += batchSize
+    # execute the commands
+    futures = []
+    for i in range(len(currentZips)):
+        filePath = currentZips[i]
+        print("Unzipping ", filePath)
+        futures.append(executor.submit(extractZip, filePath, deleteZips))
+    concurrent.futures.wait(futures)
 
-        print("One batch complete.")
-        print("---------------------------------")
+    # update the batchIndex
+    batchIndex += batchSize
 
-    print("All done!")
-else:
-    print("Okay, then goodbye!")
+    print("One batch complete.")
+    print("---------------------------------")
+
+print("All done!")
